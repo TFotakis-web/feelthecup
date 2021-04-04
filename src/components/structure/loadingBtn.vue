88dcb11dbf1e317b376e248525f896d14cadadae
TFotakis <fotakistzanis@gmail.com>
2021-04-04 22:21:37 +0300

    loadingBtn.vue: Created & Implemented

---
+++ src/components/structure/loadingBtn.vue
@@ -0,0 +1,49 @@
+<template>
+	<ion-button
+		:buttonType="buttonType"
+		:color="color"
+		:disabled="disabled"
+		:download="download"
+		:expand="expand"
+		:fill="fill"
+		:href="href"
+		:mode="mode"
+		:rel="rel"
+		:routerAnimation="routerAnimation"
+		:routerDirection="routerDirection"
+		:shape="shape"
+		:size="size"
+		:strong="strong"
+		:target="target"
+		:type="type"
+	>
+		<span>{{ loading ? loadingText : text }}</span>
+		<ion-spinner v-if="loading" class="ion-margin-start"/>
+	</ion-button>
+</template>
+<script>
+	export default {
+		name: 'LoadingBtn',
+		props: [
+			'loading',
+			'text',
+			'loadingText',
+			'buttonType',
+			'color',
+			'disabled',
+			'download',
+			'expand',
+			'fill',
+			'href',
+			'mode',
+			'rel',
+			'routerAnimation',
+			'routerDirection',
+			'shape',
+			'size',
+			'strong',
+			'target',
+			'type',
+		],
+	};
+</script>
