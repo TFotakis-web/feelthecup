fa2ab6499da02b674f661ba4484bd65553e47b6c
TFotakis <fotakistzanis@gmail.com>
2021-04-04 22:21:25 +0300

    fullScreenSpinner.vue: Created & Implemented

---
+++ src/components/structure/fullScreenSpinner.vue
@@ -0,0 +1,12 @@
+<template>
+	<div style="background: #fafafa; width: 100vw; height: 100vh; position: absolute; top: 0; left: 0; z-index: 100000;">
+		<div style="position: absolute; left: 50%; top: 50%; transform: translate(-32px, -32px); z-index: 100000;">
+			<ion-spinner/>
+		</div>
+	</div>
+</template>
+<script>
+	export default {
+		name: 'fullScreenSpinner',
+	};
+</script>
