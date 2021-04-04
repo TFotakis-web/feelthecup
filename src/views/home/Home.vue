a589d91d8fd47069f2cdc8a2fce31e73f2066a1d
TFotakis <fotakistzanis@gmail.com>
2021-04-04 23:00:52 +0300

    Home.vue: Created & Implemented

---
+++ src/views/home/Home.vue
@@ -0,0 +1,14 @@
+<template>
+	<ion-grid fixed>
+		<h1 class="ion-text-center">{{ $t('various.underConstruction') }}</h1>
+	</ion-grid>
+</template>
+<script>
+	export default {
+		name: 'Home',
+		mounted() {
+			this.$store.commit('pageStructure/setPageTitle', () => window.vm.$t('views.homePage.pageTitle'));
+			this.$store.commit('pageStructure/setPageBackButton', false);
+		},
+	};
+</script>
