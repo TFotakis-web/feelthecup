9b85879ee767719d071ff697ff734e5e69cecbb1
TFotakis <fotakistzanis@gmail.com>
2021-04-04 22:08:53 +0300

    Added project configurations

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,36 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/typescript/recommended'
-  ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'vue/no-deprecated-slot-attribute': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
-}
+	root: true,
+	env: {
+		node: true,
+	},
+	'extends': [
+		'plugin:vue/vue3-essential',
+		'eslint:recommended',
+	],
+	parserOptions: {
+		ecmaVersion: 2020,
+	},
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-unused-vars': 'warn',
+		'no-unused-labels': 'warn',
+		'no-empty': 'warn',
+		'vue/custom-event-name-casing': 'off',
+		'vue/experimental-script-setup-vars': 'off',
+		'vue/no-mutating-props': 'off',
+		'vue/no-unused-components': 'warn',
+		'vue/no-deprecated-slot-attribute': 'off'
+	},
+	overrides: [
+		{
+			files: [
+				'**/__tests__/*.{j,t}s?(x)',
+				'**/tests/unit/**/*.spec.{j,t}s?(x)',
+			],
+			env: {
+				jest: true,
+			},
+		},
+	],
+};
