91bc953188b381f58b5a97a45d798a65754e192b
TFotakis <fotakistzanis@gmail.com>
2021-04-04 22:21:51 +0300

    localeDropdown.vue: Created & Implemented

---
+++ src/components/structure/localeDropdown.vue
@@ -0,0 +1,19 @@
+<template>
+	<ion-item lines="none">
+		<ion-icon :icon="$ionicons.languageOutline" slot="start" class="ion-align-self-center"/>
+		<ion-label>{{ $t('components.localeDropdown.language') }}</ion-label>
+		<ion-select :ok-text="$t('actions.ok')" :cancel-text="$t('actions.cancel')" interface="popover" :ionChange="$i18n.$loadLanguageAsync(selectedLocale)" v-model="selectedLocale">
+			<ion-select-option v-for="locale in $i18n.availableLocales" :key="locale" :value="locale">{{ locale }}</ion-select-option>
+		</ion-select>
+	</ion-item>
+</template>
+<script>
+	export default {
+		name: 'LocaleDropdown',
+		data() {
+			return {
+				selectedLocale: this.$i18n.locale,
+			};
+		},
+	};
+</script>
