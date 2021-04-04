5182918e710524bbfc51c741ba8679dd5cbe270a
TFotakis <fotakistzanis@gmail.com>
2021-04-04 23:04:18 +0300

    BaseLayout.vue: Created & Implemented

---
+++ src/components/base/BaseLayout.vue
@@ -0,0 +1,39 @@
+<template>
+	<router-view v-slot="{ Component }">
+		<transition name="route" mode="out-in">
+			<component :is="Component"/>
+		</transition>
+	</router-view>
+</template>
+<script>
+	export default {
+		name: 'BaseLayout',
+		components: {},
+		props: {
+			pageTitle: {
+				type: String,
+				default: '',
+			},
+		},
+	};
+</script>
+<style>
+	.route-enter-from {
+		opacity: 0;
+		transform: translateY(5px) scale(0.995);
+		zoom: 0.5;
+	}
+
+	.route-enter-active {
+		transition: all 200ms ease-out;
+	}
+
+	.route-leave-active {
+		transition: all 200ms ease-in;
+	}
+
+	.route-leave-to {
+		opacity: 0;
+		transform: translateY(5px) scale(0.995);
+	}
+</style>
