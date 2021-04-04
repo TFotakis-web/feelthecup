d4784be6d4a1af4a644cd712a70000c00643c392
TFotakis <fotakistzanis@gmail.com>
2021-04-04 23:24:32 +0300

    UserProfile.vue: Created & Initialized

---
+++ src/views/userProfile/UserProfile.vue
@@ -0,0 +1,14 @@
+<template>
+	<ion-grid fixed>
+		<h1 class="ion-text-center">{{ $t('various.underConstruction') }}</h1>
+	</ion-grid>
+</template>
+<script>
+	export default {
+		name: 'UserProfile',
+		mounted() {
+			this.$store.commit('pageStructure/setPageTitle', () => window.vm.$t('views.userProfile.pageTitle'));
+			this.$store.commit('pageStructure/setPageBackButton', false);
+		},
+	};
+</script>
