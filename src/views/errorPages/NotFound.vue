a863e085bee5be971a802c574a344ea955b6c794
TFotakis <fotakistzanis@gmail.com>
2021-04-08 23:39:25 +0300

    NotFound.vue: Use ion-page & ion-content

--- src/views/errorPages/NotFound.vue
+++ src/views/errorPages/NotFound.vue
@@ -1,11 +1,15 @@
 <template>
-	<div style="text-align: center; position: absolute; left: 0; right: 0; top: 50%; transform: translateY(-50%);">
-		<h1>{{ $t('views.error404.header') }}</h1>
-		<h4>
-			<span>{{ $t('views.error404.message') + ' ' }}</span>
-			<router-link :to="{ name: 'Home' }">{{ $t('views.homePage.pageTitle') }}.</router-link>
-		</h4>
-	</div>
+	<ion-page>
+		<ion-content>
+			<div style="text-align: center; position: absolute; left: 0; right: 0; top: 50%; transform: translateY(-50%);">
+				<h1>{{ $t('views.error404.header') }}</h1>
+				<h4>
+					<span>{{ $t('views.error404.message') + ' ' }}</span>
+					<router-link :to="{ name: 'Home' }">{{ $t('views.homePage.pageTitle') }}.</router-link>
+				</h4>
+			</div>
+		</ion-content>
+	</ion-page>
 </template>
 <script>
 	export default {
