1370896466e5d39c6c650271e12fd6605b029abc
TFotakis <fotakistzanis@gmail.com>
2021-04-08 23:39:34 +0300

    Map.vue: Created & Initialized

---
+++ src/views/map/Map.vue
@@ -0,0 +1,19 @@
+<template>
+	<base-layout :page-title="$t('views.map.pageTitle')">
+		<h1 class="ion-text-center">{{ $t('various.underConstruction') }}</h1>
+	</base-layout>
+</template>
+<script>
+	import BaseLayout from '@/components/base/BaseLayout';
+
+	export default {
+		name: 'Map',
+		components: {
+			BaseLayout,
+		},
+		mounted() {
+			this.$store.commit('pageStructure/setPageTitle', () => window.vm.$t('views.map.pageTitle'));
+			this.$store.commit('pageStructure/setPageBackButton', false);
+		},
+	};
+</script>
