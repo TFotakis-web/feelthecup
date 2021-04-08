167443f69b5387587d5dc71efa04187373a62b02
TFotakis <fotakistzanis@gmail.com>
2021-04-08 23:40:22 +0300

    various.js: Created routes for maps, qrcode & rewards

--- src/plugins/router/various.js
+++ src/plugins/router/various.js
@@ -8,6 +8,30 @@ export default [
 		},
 	},
 	{
+		path: '/map',
+		name: 'Map',
+		component: () => import('@/views/map/Map'),
+		meta: {
+			requiresAuth: true,
+		},
+	},
+	{
+		path: '/qrCode',
+		name: 'QRCode',
+		component: () => import('@/views/qrCode/QRCode'),
+		meta: {
+			requiresAuth: true,
+		},
+	},
+	{
+		path: '/rewards',
+		name: 'Rewards',
+		component: () => import('@/views/rewards/Rewards'),
+		meta: {
+			requiresAuth: true,
+		},
+	},
+	{
 		path: '/profile',
 		name: 'UserProfile',
 		component: () => import('@/views/userProfile/UserProfile'),
