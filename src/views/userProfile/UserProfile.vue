aa850d3410bb5305cf7ad68962abadbfa9093c4e
TFotakis <fotakistzanis@gmail.com>
2021-04-08 23:39:01 +0300

    UserProfile.vue: Use BaseLayout

--- src/views/userProfile/UserProfile.vue
+++ src/views/userProfile/UserProfile.vue
@@ -1,11 +1,16 @@
 <template>
-	<ion-grid fixed>
+	<base-layout :page-title="$t('views.userProfile.pageTitle')">
 		<h1 class="ion-text-center">{{ $t('various.underConstruction') }}</h1>
-	</ion-grid>
+	</base-layout>
 </template>
 <script>
+	import BaseLayout from '@/components/base/BaseLayout';
+
 	export default {
 		name: 'UserProfile',
+		components: {
+			BaseLayout,
+		},
 		mounted() {
 			this.$store.commit('pageStructure/setPageTitle', () => window.vm.$t('views.userProfile.pageTitle'));
 			this.$store.commit('pageStructure/setPageBackButton', false);
