8b96e773ccf30fe8e584526a00026d8b74a27eac
TFotakis <fotakistzanis@gmail.com>
2021-04-13 16:40:32 +0300

    SetReminder.vue: Created & Initialized

---
+++ src/views/reminder/SetReminder.vue
@@ -0,0 +1,19 @@
+<template>
+	<base-layout :page-title="$t('views.setReminder.pageTitle')">
+		<h1 class="ion-text-center">{{ $t('various.underConstruction') }}</h1>
+	</base-layout>
+</template>
+<script>
+	import BaseLayout from '@/components/base/BaseLayout';
+
+	export default {
+		name: 'SetReminder',
+		components: {
+			BaseLayout,
+		},
+		mounted() {
+			this.$store.commit('pageStructure/setPageTitle', () => window.vm.$t('views.setReminder.pageTitle'));
+			this.$store.commit('pageStructure/setPageBackButton', false);
+		},
+	};
+</script>
