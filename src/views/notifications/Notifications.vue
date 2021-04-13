ad7593e880eae73abdfbb6964a82f807c8f18a0b
TFotakis <fotakistzanis@gmail.com>
2021-04-13 15:38:22 +0300

    Notifications.vue: Created & Initialized

---
+++ src/views/notifications/Notifications.vue
@@ -0,0 +1,19 @@
+<template>
+	<base-layout :page-title="$t('views.notifications.pageTitle')">
+		<h1 class="ion-text-center">{{ $t('various.underConstruction') }}</h1>
+	</base-layout>
+</template>
+<script>
+	import BaseLayout from '@/components/base/BaseLayout';
+
+	export default {
+		name: 'Notifications',
+		components: {
+			BaseLayout,
+		},
+		mounted() {
+			this.$store.commit('pageStructure/setPageTitle', () => window.vm.$t('views.notifications.pageTitle'));
+			this.$store.commit('pageStructure/setPageBackButton', false);
+		},
+	};
+</script>
