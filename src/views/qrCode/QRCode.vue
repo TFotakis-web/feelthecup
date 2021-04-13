8d27bd1893c8b5ca30bb0beae5965d702dfa3168
TFotakis <fotakistzanis@gmail.com>
2021-04-13 17:47:22 +0300

    QRCode.vue: Implemented layout

--- src/views/qrCode/QRCode.vue
+++ src/views/qrCode/QRCode.vue
@@ -1,15 +1,53 @@
 <template>
-	<base-layout :page-title="$t('views.qrcode.pageTitle')">
-		<h1 class="ion-text-center">{{ $t('various.underConstruction') }}</h1>
+	<base-layout :page-title="$t('views.qrcode.pageTitle')" content-color="primary">
+		<ion-row>
+			<ion-col class="ion-text-center">
+				<h1 class="ion-text-center ion-margin-bottom">{{ $t('actions.return') }}</h1>
+				<div class="ion-padding ion-margin" style="background-color: white; border-radius: 10px; width: fit-content; display: inline-block">
+					<qrcode-vue :value="qrCodeUrl" :size="170" level="H"/>
+				</div>
+			</ion-col>
+		</ion-row>
+		<ion-row>
+			<ion-col size="12" class="ion-text-center">
+				<ion-item color="primary" class="ion-text-center" style="border: 1px solid black; border-radius: 10px; width: fit-content; display: inline-block">
+					<ion-icon :icon="$ionicons.giftOutline" size="large" class="ion-align-self-center"/>
+					<div class="ion-padding" style="display: inline-block">
+						<h6 class="ion-no-margin"><strong>{{ pointsText }}</strong></h6>
+						<h6 class="ion-no-margin">for the next</h6>
+						<h6 class="ion-no-margin"><strong>{{ timeText }}</strong></h6>
+					</div>
+				</ion-item>
+			</ion-col>
+			<ion-col size="12" class="ion-text-center">
+				<ion-item color="primary" class="ion-text-center" style="border: 1px solid black; border-radius: 10px; width: fit-content; display: inline-block">
+					<ion-icon :icon="$ionicons.timeOutline" size="large" class="ion-align-self-center"/>
+					<div class="ion-padding" style="display: inline-block">
+						<h6 class="ion-no-margin"><strong>{{ $t('views.qrcode.timeInUse') }}</strong></h6>
+						<h6 class="ion-no-margin"><strong>{{ timeInUse }}</strong></h6>
+					</div>
+				</ion-item>
+			</ion-col>
+		</ion-row>
 	</base-layout>
 </template>
 <script>
 	import BaseLayout from '@/components/base/BaseLayout';
+	import QrcodeVue from 'qrcode.vue';
 
 	export default {
 		name: 'QRCode',
 		components: {
 			BaseLayout,
+			QrcodeVue,
+		},
+		data() {
+			return {
+				qrCodeUrl: 'www.example.com',
+				pointsText: '+100 points',
+				timeText: '20:05 min',
+				timeInUse: '45:00',
+			};
 		},
 		mounted() {
 			this.$store.commit('pageStructure/setPageTitle', () => window.vm.$t('views.qrcode.pageTitle'));
