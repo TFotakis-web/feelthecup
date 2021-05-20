3d1264c49c49696bbbed3fe1d107f60b7b86bb48
alexnikas <alex-nikas@hotmail.com>
2021-05-20 22:13:26 +0300

    Post confirmation trigger initialized!

--- amplify/backend/function/feelthecupProdPostConfirmation/src/custom.js
+++ amplify/backend/function/feelthecupProdPostConfirmation/src/custom.js
@@ -1,4 +1,36 @@
+var AWS = require('aws-sdk')
+
+AWS.config.update({ region: process.env.REGION })
+
+var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+
 exports.handler = (event, context, callback) => {
-  // insert code to be executed by your lambda trigger
+  var params = {
+  TableName: 'User-sjl4hvfvy5czriadkhqxmzyape-prod', //TODO to be changed to process.env...
+  Item: {
+    'id' : {
+      S: event.request.userAttributes.sub
+    },
+    'name' : {S: event.request.userAttributes.name},
+    'surname' : {S: event.request.userAttributes.family_name},
+    'email' : {S: event.request.userAttributes.email},
+    'mobileNumber' : {S: event.request.userAttributes.phone_number},
+    'birthdate' : {S: event.request.userAttributes.birthdate},
+    'cup_balance' : {S: '0'},
+    'credits' : {S: '0'},
+    'loyaltyBalance' : {S: '0'},
+    'noOfCupsLost' : {S: '0'},
+    'address' : {S: 'Null'}
+    }
+  };
+  
+  ddb.putItem(params, function(err, data) {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log("Success", data);
+    }
+  });
   callback(null, event);
 };
---
+++ src/models/index.js
@@ -0,0 +1,30 @@
+// @ts-check
+import { initSchema } from '@aws-amplify/datastore';
+import { schema } from './schema';
+
+const TransactionStatus = {
+  "RECEIVED": "RECEIVED",
+  "RETURNED": "RETURNED",
+  "LOST": "LOST"
+};
+
+const RewardType = {
+  "ON_TIME_RETURN": "ON_TIME_RETURN",
+  "SUGGESTED_LOCATION_RETURN": "SUGGESTED_LOCATION_RETURN"
+};
+
+const { User, Store, Transaction, Address, Deposit, Contract, Plan, Invoice, Reward } = initSchema(schema);
+
+export {
+  User,
+  Store,
+  Transaction,
+  TransactionStatus,
+  RewardType,
+  Address,
+  Deposit,
+  Contract,
+  Plan,
+  Invoice,
+  Reward
+};
