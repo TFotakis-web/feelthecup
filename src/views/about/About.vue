3da58dc6cfdae7b9f18f8c03078b19dbeb679dd5
TFotakis <fotakistzanis@gmail.com>
2021-05-20 19:59:11 +0300

    About.vue: Created & Initialized

---
+++ src/views/about/About.vue
@@ -0,0 +1,20 @@
+<template>
+	<base-layout :page-title="$t('views.about.pageTitle')">
+		<h1 class="ion-text-center">{{ $t('various.underConstruction') }}</h1>
+	</base-layout>
+</template>
+<script>
+	import BaseLayout from '@/components/base/BaseLayout';
+
+	export default {
+		name: 'About',
+		components: {
+			BaseLayout,
+		},
+		mounted() {
+			this.$store.commit('pageStructure/setPageTitle', () => window.vm.$t('views.about.pageTitle'));
+			this.$store.commit('pageStructure/setPageBackButton', true);
+			this.$store.commit('pageStructure/setBackButtonDefaultHref', this.$router.resolve({ name: 'UserProfile' }).fullPath);
+		},
+	};
+</script>
