c6edc0f30b4abaa45c6c2a630fa05d6ccb8a0bdf
TFotakis <fotakistzanis@gmail.com>
2021-06-01 17:34:52 +0300

    StoreSettings.vue: Created & Initialized UI

---
+++ src/views/storeSettings/StoreSettings.vue
@@ -0,0 +1,21 @@
+<template>
+	<base-layout :page-title="$t('views.storeSettings.pageTitle')">
+		<h1 class="ion-text-center">{{ $t('various.underConstruction') }}</h1>
+	</base-layout>
+</template>
+<script>
+	import BaseLayout from '@/components/base/BaseLayout';
+
+
+	export default {
+		name: 'StoreSettings',
+		components: {
+			BaseLayout,
+		},
+		mounted() {
+			this.$store.commit('pageStructure/setPageTitle', () => window.vm.$t('views.storeSettings.pageTitle'));
+			this.$store.commit('pageStructure/setPageBackButton', true);
+			this.$store.commit('pageStructure/setBackButtonDefaultHref', this.$router.resolve({ name: 'StoreHome' }).fullPath);
+		},
+	};
+</script>
