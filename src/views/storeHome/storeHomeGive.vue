69a29e5b0fa95c6a7b5cda87439b39a69f87d93e
TFotakis <fotakistzanis@gmail.com>
2021-06-01 19:05:21 +0300

    storeHomeGive.vue: Added qrcode-stream component & qrcode-reader implementation in comments

--- src/views/storeHome/storeHomeGive.vue
+++ src/views/storeHome/storeHomeGive.vue
@@ -22,11 +22,43 @@
 				</ion-button>
 			</ion-col>
 		</ion-row>
+<!--		<ion-row v-if="src !== null">-->
+<!--			<ion-col>-->
+<!--				<ion-img :src="src"/>-->
+<!--			</ion-col>-->
+<!--		</ion-row>-->
+		<ion-row>
+			<ion-col>
+<!--				<h1>QR Stream</h1>-->
+				<p>{{ userId }}</p>
+				<qrcode-stream @decode="onDecode" @init="onInit" />
+			</ion-col>
+		</ion-row>
 	</ion-grid>
 </template>
 <script>
+	// import { Camera, CameraResultType } from '@capacitor/camera';
+	// import QrCode from 'qrcode-reader';
+
+	import { QrcodeStream } from 'vue3-qrcode-reader';
+
+
 	export default {
 		name: 'storeHomeGive',
+		components: {
+			QrcodeStream
+		},
+		data() {
+			return {
+				src: null,
+				userId: '',
+				result: '',
+				error: ''
+			};
+		},
+		// created() {
+		// 	Camera.requestPermissions();
+		// },
 		methods: {
 			confirmNewCustomer() {
 				console.log('Confirm new customer.');
@@ -34,8 +66,49 @@
 			cupLost() {
 				console.log('Cup Lost.');
 			},
-			qrScan() {
+			async qrScan() {
 				console.log('QR Scan');
+				//
+				// const image = await Camera.getPhoto({
+				// 	quality: 100,
+				// 	allowEditing: false,
+				// 	resultType: CameraResultType.DataUrl,
+				// });
+				//
+				// this.src = image.dataUrl;
+				//
+				// let qr = new QrCode();
+				// qr.callback = function (err, value) {
+				// 	if (err) {
+				// 		return console.error(err);
+				// 	}
+				// 	this.userId = value.result;
+				// 	console.log(value.result);
+				// };
+				// qr.decode(this.src);
+			},
+			onDecode (result) {
+				this.userId = result;
+				console.log(result);
+			},
+			async onInit (promise) {
+				try {
+					await promise
+				} catch (error) {
+					if (error.name === 'NotAllowedError') {
+						this.error = "ERROR: you need to grant camera access permission"
+					} else if (error.name === 'NotFoundError') {
+						this.error = "ERROR: no camera on this device"
+					} else if (error.name === 'NotSupportedError') {
+						this.error = "ERROR: secure context required (HTTPS, localhost)"
+					} else if (error.name === 'NotReadableError') {
+						this.error = "ERROR: is the camera already in use?"
+					} else if (error.name === 'OverconstrainedError') {
+						this.error = "ERROR: installed cameras are not suitable"
+					} else if (error.name === 'StreamApiNotSupportedError') {
+						this.error = "ERROR: Stream API is not supported in this browser"
+					}
+				}
 			}
 		},
 	};
