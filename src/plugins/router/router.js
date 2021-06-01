9403ad85ebe8fdefd28fb8d202cd3200de73ca89
TFotakis <fotakistzanis@gmail.com>
2021-06-01 17:35:37 +0300

    router.js: Implemented store routes and permissions

--- src/plugins/router/router.js
+++ src/plugins/router/router.js
@@ -5,12 +5,14 @@ import { store } from '@/plugins/store/store';
 import auth from '@/plugins/router/auth';
 import tabs from '@/plugins/router/tabs';
 import various from '@/plugins/router/various';
+import storeRoutes from '@/plugins/router/store';
 
 
 const routes = [
 	...auth,
 	...tabs,
 	...various,
+	...storeRoutes,
 	{
 		path: '/:catchAll(.*)',
 		name: 'NotFound',
@@ -28,12 +30,20 @@ function hasPermissions(route) {
 		return route.meta.isAdmin;
 	}
 
+	if (store.getters['auth/isStore']) {
+		return route.meta.isStore;
+	}
+
 	let flag = true;
 	const userModelPermissions = store.getters['auth/userModelPermissions'];
 	const userPagePermissions = store.getters['auth/userPagePermissions'];
 
 	if (route.meta.isAdmin) {
 		flag &= store.getters['auth/isAdmin'];
+	}
+
+	if (route.meta.isStore) {
+		flag &= store.getters['auth/isStore'];
 	}
 
 	if (route.meta.requiresPagePermission) {
@@ -66,7 +76,15 @@ router.hasPermissionsByName = hasPermissionsByName;
 
 function checkRoute(to, from, next) {
 	let user = store.getters['auth/user'];
-	const defaultRoute = () => ({ name: store.getters['auth/isAdmin'] ? 'AdminHome' : 'Home' });
+	const defaultRoute = () => {
+		if (store.getters['auth/isAdmin']) {
+			return { name: 'AdminHome' };
+		}
+		if (store.getters['auth/isStore']) {
+			return { name: 'StoreHome' };
+		}
+		return { name: 'Home' };
+	};
 	if (to.meta.requiresAuth) {
 		if (!user) {
 			return store.dispatch('auth/currentAuthenticatedUser')
