0374b3a73beb82fd53001e7b06e7026fbb037b40
TFotakis <fotakistzanis@gmail.com>
2021-06-03 13:01:00 +0300

    StoreSettings.vue: Added signOut button

--- src/views/storeSettings/StoreSettings.vue
+++ src/views/storeSettings/StoreSettings.vue
@@ -1,6 +1,12 @@
 <template>
 	<base-layout :page-title="$t('views.storeSettings.pageTitle')">
 		<h1 class="ion-text-center">{{ $t('various.underConstruction') }}</h1>
+		<ion-list>
+			<ion-item @click="$store.dispatch('auth/signOut')" button>
+				<ion-icon slot="start" :icon="$ionicons.logOutOutline" class="ion-align-self-center"/>
+				<ion-label>{{ $t('actions.signOut') }}</ion-label>
+			</ion-item>
+		</ion-list>
 	</base-layout>
 </template>
 <script>
