be3b2b1e969f1797836cca4b7caefa546f8e8ede
TFotakis <fotakistzanis@gmail.com>
2021-06-03 13:02:22 +0300

    SignIn.vue: UI Redesign

--- src/views/auth/SignIn.vue
+++ src/views/auth/SignIn.vue
@@ -1,42 +1,31 @@
 <template>
 	<ion-page>
-		<ion-content>
-			<ion-grid fixed>
+		<ion-content :fullscreen="true" color="gradient">
+			<ion-grid fixed class="ion-centered-container">
 				<ion-row class="ion-justify-content-center">
 					<ion-col size-sm="10" size-md="8" size-lg="6" size-xl="6">
-						<ion-card>
-							<ion-card-header>
-								<ion-card-title>{{ $t('views.auth.signInToYourFeelTheCupAccount') }}</ion-card-title>
-							</ion-card-header>
-							<ion-card-content>
-								<form @submit.prevent="signIn">
-									<ion-item>
-										<ion-icon :icon="$ionicons.personOutline" slot="start" class="ion-align-self-center"/>
-										<ion-label position="floating">{{ $t('fields.username') }}</ion-label>
-										<ion-input v-model="credentials.username" type="text" name="username" autocomplete="username" required/>
-									</ion-item>
-									<ion-item>
-										<ion-icon :icon="$ionicons.keyOutline" slot="start" class="ion-align-self-center"/>
-										<ion-label position="floating">{{ $t('fields.password') }}</ion-label>
-										<ion-input v-model="credentials.password" :type="passwordVisible ? 'text' : 'password'" name="password" autocomplete="current-password" required/>
-										<ion-button @click="passwordVisible = !passwordVisible" slot="end" fill="clear" class="ion-align-self-center">
-											<ion-icon slot="icon-only" :icon="passwordVisible ? $ionicons.eyeOffOutline : $ionicons.eyeOutline"/>
-										</ion-button>
-									</ion-item>
-									<router-link :to="{ name: 'ForgotPassword' }" class="ion-margin-vertical">{{ $t('views.auth.forgotYourPassword') }}</router-link>
-									<div class="ion-margin-top">
-										<loadingBtn color="accent" expand="block" type="submit" :loading="loading" :text="$t('views.auth.signIn')" :loadingText="$t('views.auth.signingIn')" class="ion-margin-bottom"/>
-										<p v-if="error !== {}" class="text-danger">{{ error.message }}</p>
-										<hr class="ion-margin-vertical"/>
-										<p>
-											<span>{{ $t('views.auth.dontHaveAnAccount') + ' ' }}</span>
-											<router-link :to="{ name: 'SignUp' }">{{ $t('views.auth.createAccount') }}</router-link>
-										</p>
-										<localeDropdown/>
-									</div>
-								</form>
-							</ion-card-content>
-						</ion-card>
+						<ion-icon icon="/assets/logo.svg" color="light" class="ion-margin-bottom" style="font-size: 200px"/>
+						<h1 class="ion-margin-bottom" style="font-size: xxx-large">{{ $t('views.auth.enter') }}</h1>
+						<form @submit.prevent="signIn">
+							<ion-item class="ion-item-round ion-margin-bottom">
+								<ion-icon :icon="$ionicons.personOutline" slot="start" class="ion-align-self-center"/>
+								<ion-label position="floating">{{ $t('fields.username') }}</ion-label>
+								<ion-input v-model="credentials.username" type="text" name="username" autocomplete="username" required/>
+							</ion-item>
+							<ion-item class="ion-item-round ion-margin-bottom">
+								<ion-icon :icon="$ionicons.keyOutline" slot="start" class="ion-align-self-center"/>
+								<ion-label position="floating">{{ $t('fields.password') }}</ion-label>
+								<ion-input v-model="credentials.password" :type="passwordVisible ? 'text' : 'password'" name="password" autocomplete="current-password" required/>
+								<ion-button @click="passwordVisible = !passwordVisible" slot="end" fill="clear" color="dark" class="ion-align-self-center">
+									<ion-icon slot="icon-only" :icon="passwordVisible ? $ionicons.eyeOffOutline : $ionicons.eyeOutline"/>
+								</ion-button>
+							</ion-item>
+							<loadingBtn color="dark" expand="block" type="submit" shape="round" :loading="loading" :text="$t('views.auth.signIn')" :loadingText="$t('views.auth.signingIn')" class="ion-margin-bottom"/>
+							<p v-if="error !== {}" class="text-danger">{{ error.message }}</p>
+							<ion-button :router-link="{ name: 'ForgotPassword' }" fill="clear" color="dark" expand="block">{{ $t('views.auth.forgotYourPassword') }}</ion-button>
+							<ion-button :router-link="{ name: 'SignUp' }" fill="clear" color="dark" expand="block">{{ $t('views.auth.createAccount') }}</ion-button>
+							<localeDropdown :use-button="true" fill="clear" color="dark" expand="block"/>
+						</form>
 					</ion-col>
 				</ion-row>
 			</ion-grid>
@@ -90,3 +79,9 @@
 		},
 	};
 </script>
+<style>
+	.gradient-background {
+		background: rgb(220, 106, 53);
+		background: linear-gradient(45deg, rgba(220, 106, 53, 1) 0%, rgba(244, 206, 16, 1) 50%, rgba(97, 191, 166, 1) 100%);
+	}
+</style>
