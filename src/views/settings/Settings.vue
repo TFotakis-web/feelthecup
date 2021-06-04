bbdffd31fa2baf34a0fc5778255cbb339b8e985f
TFotakis <fotakistzanis@gmail.com>
2021-06-04 17:11:15 +0300

    Settings.vue: Added signOut, localeDropdown & some list headers

--- src/views/settings/Settings.vue
+++ src/views/settings/Settings.vue
@@ -1,15 +1,32 @@
 <template>
 	<base-layout :page-title="$t('views.settings.pageTitle')">
-		<h1 class="ion-text-center">{{ $t('various.underConstruction') }}</h1>
+		<ion-list>
+			<ion-list-header style="font-size: x-large; font-weight: bold;">
+				<ion-label>{{ $t('views.settings.generalGroup') }}</ion-label>
+			</ion-list-header>
+			<ion-list-header style="font-size: x-large; font-weight: bold;">
+				<ion-label>{{ $t('views.settings.accountGroup') }}</ion-label>
+			</ion-list-header>
+			<ion-list-header style="font-size: x-large; font-weight: bold;">
+				<ion-label>{{ $t('views.settings.appGroup') }}</ion-label>
+			</ion-list-header>
+			<locale-dropdown lines="inset"/>
+			<ion-item @click="$store.dispatch('auth/signOut')" button>
+				<ion-icon slot="start" :icon="$ionicons.logOutOutline" color="danger" class="ion-align-self-center"/>
+				<ion-label color="danger">{{ $t('actions.signOut') }}</ion-label>
+			</ion-item>
+		</ion-list>
 	</base-layout>
 </template>
 <script>
 	import BaseLayout from '@/components/base/BaseLayout';
+	import LocaleDropdown from '@/components/structure/localeDropdown';
 
 
 	export default {
 		name: 'Settings',
 		components: {
+			LocaleDropdown,
 			BaseLayout,
 		},
 		mounted() {
