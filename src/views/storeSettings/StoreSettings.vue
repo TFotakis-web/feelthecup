bc59b1f739782a69086dc70886d41a62a7ccf998
TFotakis <fotakistzanis@gmail.com>
2021-06-04 17:10:46 +0300

    StoreSettings.vue: Added localeDropdown

--- src/views/storeSettings/StoreSettings.vue
+++ src/views/storeSettings/StoreSettings.vue
@@ -1,21 +1,23 @@
 <template>
 	<base-layout :page-title="$t('views.storeSettings.pageTitle')">
-		<h1 class="ion-text-center">{{ $t('various.underConstruction') }}</h1>
 		<ion-list>
+			<locale-dropdown lines="inset"/>
 			<ion-item @click="$store.dispatch('auth/signOut')" button>
-				<ion-icon slot="start" :icon="$ionicons.logOutOutline" class="ion-align-self-center"/>
-				<ion-label>{{ $t('actions.signOut') }}</ion-label>
+				<ion-icon slot="start" :icon="$ionicons.logOutOutline" color="danger" class="ion-align-self-center"/>
+				<ion-label color="danger">{{ $t('actions.signOut') }}</ion-label>
 			</ion-item>
 		</ion-list>
 	</base-layout>
 </template>
 <script>
 	import BaseLayout from '@/components/base/BaseLayout';
+	import LocaleDropdown from '@/components/structure/localeDropdown';
 
 
 	export default {
 		name: 'StoreSettings',
 		components: {
+			LocaleDropdown,
 			BaseLayout,
 		},
 		mounted() {
